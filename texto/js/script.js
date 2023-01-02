bbf2cf72d8a9b022a50ba85758dd73d21c7ea77d
fernandodearrillaga <115628468+fernandodearrillaga@users.noreply.github.com>
2023-01-02 18:34:15 +0100

    02/01/2023 18:34

--- texto/js/script.js
+++ texto/js/script.js
@@ -1,5 +1,8 @@
 let jugadores=0;
-let dados =[{valor: 1}, {valor: 1}]
+let fichas=50;
+let turno =1;
+let dados =[{valor: 1}, {valor: 1}];
+let arrJugadores=["0"];
 window.onload = function(){
     document.getElementById('tirar').onclick = tirarDados;
 
@@ -8,7 +11,16 @@ window.onload = function(){
 while (jugadores>5||jugadores<1) {
     jugadores=parseInt(prompt("Introducir número de jugadores entre 1 y 5"));
 }
-
+for (let i =0; i<jugadores; i++){
+    crearJugador();
+}
+function crearJugador(){
+    let jugador={fichasRestantes: parseInt(fichas/jugadores),
+                 fichasGanadas: 0
+                };
+    arrJugadores.push(jugador);
+    
+}
 
 function tirarDados(){
     dados[0].valor=Math.floor(Math.random()*6+1);
@@ -16,10 +28,22 @@ function tirarDados(){
     let suma =dados[0].valor+dados[1].valor;
     console.log(dados[0].valor+dados[1].valor);
     const dado = document.getElementById('dados');
-    dado.innerText = dados[0].valor + "+" + dados[1].valor + "=" +  suma;
+    dado.innerText ="Dados: "+ dados[0].valor + " + " + dados[1].valor + " = " +  suma;
+    
+    const textoTurno = document.getElementById('turno');
+    
+    textoTurno.innerText= "Turno del jugador " + turno;
+    turno++;
+    if (turno>jugadores){
+        turno=1;
+    }
+    const siguiente = document.getElementById('siguiente');
+    
+    siguiente.innerText= "Tira el jugador " + turno;
 }
 
 
 //tirarDados();
 //console.log(dados[0].valor+dados[1].valor);
 console.log(jugadores);
+console.log(arrJugadores);
