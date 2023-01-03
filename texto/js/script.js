8b5d9926706cd2ad11eb39a2e898da5b9aea154e
fernandodearrillaga <115628468+fernandodearrillaga@users.noreply.github.com>
2023-01-03 21:05:30 +0100

    03/01/23 21:05

--- texto/js/script.js
+++ texto/js/script.js
@@ -3,8 +3,11 @@ let fichas=50;
 let turno =1;
 let dados =[{valor: 1}, {valor: 1}];
 let arrJugadores=["0"];
+let arrCasillas=[]
+let puchero={contenido: 0};
 window.onload = function(){
     escribirJugadores();
+    escribirCasillas();
     document.getElementById('tirar').onclick = tirarDados;
 
 }
@@ -15,6 +18,12 @@ while (jugadores>5||jugadores<1) {
 for (let i =0; i<jugadores; i++){
     crearJugador();
 }
+
+
+    crearCasillas();
+
+
+
 function crearJugador(){
     let jugador={fichasRestantes: parseInt(fichas/jugadores),
                  fichasGanadas: 0
@@ -26,9 +35,40 @@ function crearJugador(){
 function escribirJugadores() {
     
     const escribirJugadores = document.getElementById('jugadores');
-    escribirJugadores.innerText="";
+    escribirJugadores.innerText="Jugadores";
     for (let i=1; i<=jugadores; i++){
         escribirJugadores.innerText +="\n Jugador " + i + "\n Fichas ganadas: " + arrJugadores[i].fichasGanadas + "\n Fichas restantes: "  + arrJugadores[i].fichasRestantes;
+    }
+    
+}
+
+function crearCasillas(){
+    let casilla;
+    for (let i = 2; i <= 11; i++ ){
+            casilla={contenido: 0,
+                    capacidad: i
+                    };
+        
+        if(i!=7){
+            arrCasillas.push(casilla);
+        }
+        
+                    
+    }
+ 
+    
+    
+    
+}
+
+function escribirCasillas() {
+    
+    const escribirCasillas = document.getElementById('casillas');
+    escribirCasillas.innerText="Casillas";
+    escribirCasillas.innerText += "\n Puchero \n" + puchero.contenido;
+    for (let i=0; i<arrCasillas.length; i++){
+        
+        escribirCasillas.innerText +="\n Casilla " + arrCasillas[i].capacidad + "\n" + arrCasillas[i].contenido + "/" + arrCasillas[i].capacidad;
     }
     
 }
@@ -50,11 +90,17 @@ function tirarDados(){
     console.log(dados[0].valor+dados[1].valor);
     const dado = document.getElementById('dados');
     dado.innerText ="Dados: "+ dados[0].valor + " + " + dados[1].valor + " = " +  suma;
+    addFicha(suma);
+
+
     
     const textoTurno = document.getElementById('turno');
     
     textoTurno.innerText= "Turno del jugador " + turno;
-    arrJugadores[turno].fichasRestantes--;
+    if (arrJugadores[turno].fichasRestantes!==0){
+        arrJugadores[turno].fichasRestantes--;
+    }
+    
     escribirJugadores();
     turno++;
     if (turno>jugadores){
@@ -65,9 +111,32 @@ function tirarDados(){
     siguiente.innerText= "Tira el jugador " + turno;
 }
 
+function addFicha(suma){
+    for (let i=0;i<arrCasillas.length;i++){
+        if (arrCasillas[i].capacidad==suma){
+            arrCasillas[i].contenido++;
+        }
+        //console.log(arrCasillas[i].contenido,arrCasillas[i].capacidad)
+        if (arrCasillas[i].contenido==arrCasillas[i].capacidad){
+            arrJugadores[turno].fichasGanadas=arrCasillas[i].contenido+arrJugadores[turno].fichasGanadas;
+            arrCasillas[i].contenido=0;
+        }
+    }
+    if (suma==7){
+        puchero.contenido++;
+    }
+    if (suma==12){
+        arrJugadores[turno].fichasRestantes++;
+        arrJugadores[turno].fichasGanadas=puchero.contenido+arrJugadores[turno].fichasGanadas;
+        puchero.contenido=0;
+    }
+    escribirCasillas();
+    
+}
 
 //tirarDados();
 //console.log(dados[0].valor+dados[1].valor);
 console.log(jugadores);
 console.log(arrJugadores);
+console.log(arrCasillas);
 //escribirJugadores();
