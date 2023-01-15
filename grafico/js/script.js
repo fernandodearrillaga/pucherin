1ae50e80c8cdb2c60a4e35cee6761dbee7cf9a4f
fernandodearrillaga <115628468+fernandodearrillaga@users.noreply.github.com>
2023-01-15 19:37:42 +0100

    15/01/23

--- grafico/js/script.js
+++ grafico/js/script.js
@@ -1,11 +1,136 @@
+var container = document.querySelector(".box");
+const casillas = 9;
+
+// crear puchero
+ var puchero = document.createElement('canvas');
+ puchero.classList.add('puchero');
+ puchero.width = 120;
+ puchero.height = 120;
+ container.appendChild(puchero);
+ pintarPuchero(puchero);
+/*<!-- Crear elementos canvas para las casillas -->
+<!-- Usar un bucle for para crear los elementos de manera automática -->
+
+<!-- Los canvas se formarán en una elipse  -->*/
+for (var i = 0; i < casillas; i++) {
+  // Crear un elemento canvas
+  var canvas = document.createElement('canvas');
+  canvas.classList.add('casilla');
+  // Establecer el ancho y alto del canvas en 50 (cada canvas será de 50 x 50)
+  canvas.width = 120;
+  canvas.height = 120;
+  // Añadir el canvas a la página
+  container.appendChild(canvas);
+  
+}
+
+// Obtener una referencia a todos los elementos canvas en la página
+var canvases = document.querySelectorAll('.casilla');
+
+
+
+// Dibujar una elipse en cada canvas y posicionarlos en una elipse de 
+for (var i = 0; i < canvases.length; i++) {
+  // Obtener el contexto del canvas en 2D
+  var ctx = canvases[i].getContext('2d');
+
+  // Dibujar una elipse en el canvas
+  ctx.beginPath();  
+  
+  //ctx.ellipse(35, 35, 35, 35, 0, 0, 2 * Math.PI);
+  //ctx.stroke();
+
+  // Posicionar el canvas en la elipse 
+  canvases[i].style.left = Math.cos(2 * Math.PI * i / casillas) * 400 + 400 - 25 + 'px';
+  canvases[i].style.top = Math.sin(2 * Math.PI * i / casillas) * 250 + 300  - 25  + 'px';
+  
+  if (i>4) pintarCasilla(canvases[i],i+3)
+  else pintarCasilla(canvases[i],i+2)
+}
+
+
+
+function pintarCasilla(canvas, fichas, num){
+  var ctx = canvas.getContext('2d');
+  ctx.arc(canvas.width / 2, canvas.height / 2, 60, 0, 2 * Math.PI);
+  ctx.fillStyle = 'green';
+  ctx.fill();
+  for (var i = 0; i < fichas; i++) {
+      // Calcular la posición en el círculo para cada ficha
+      var x = Math.cos(2 * Math.PI * i / fichas) * 35 + canvas.width / 2;
+      var y = Math.sin(2 * Math.PI * i / fichas) * 35 + canvas.height / 2;
+
+      // Dibujar la ficha en la posición calculada
+      ctx.beginPath();
+      ctx.arc(x, y, 9, 0, 2 * Math.PI);
+      if (i < num ) ctx.fillStyle = 'red'
+         else ctx.fillStyle = 'white'
+      ctx.fill();
+    
+     ctx.fillStyle = 'white';
+    // Establecer la fuente para el texto
+     ctx.font = '24px sans-serif';
+     // Dibujar el número en el canvas usando el método fillText()
+     x = canvas.width / 2 - ctx.measureText(fichas).width / 2;
+     y = canvas.height / 2 + 10;
+     ctx.fillText(fichas, x, y);
+  }
+  
+}
+
+function pintarPuchero(canvas){
+  var ctx = canvas.getContext('2d');
+  ctx.arc(canvas.width / 2, canvas.height / 2, 60, 0, 2 * Math.PI);
+  ctx.fillStyle = 'red';
+  ctx.fill();
+  for (var i = 0; i < 4; i++) {
+      // Calcular la posición en el círculo para cada ficha
+      var x = Math.cos(2 * Math.PI * i / 4) * 35 + canvas.width / 2;
+      var y = Math.sin(2 * Math.PI * i / 4) * 35 + canvas.height / 2;
+
+      // Dibujar la ficha en la posición calculada
+      ctx.beginPath();
+      ctx.arc(x, y, 9, 0, 2 * Math.PI);
+     
+      ctx.fillStyle = 'white'
+      ctx.fill();
+    
+    
+     ctx.fillStyle = 'white';
+    // Establecer la fuente para el texto
+     ctx.font = '24px sans-serif';
+     // Dibujar el número en el canvas usando el método fillText()
+     x = canvas.width / 2 - ctx.measureText(7).width / 2;
+     y = canvas.height / 2 + 10;
+     ctx.fillText(7, x, y);
+  }
+  
+}
+
+// pintamos fichas aleatorias en el tablero
+/*for(let i=0; i < canvases.length; i++){
+  let fichas = Math.ceil(Math.random()*(i+2));
+   if (i>4) pintarCasilla(canvases[i],i+3,fichas)
+  else pintarCasilla(canvases[i],i+2,fichas)
+
+  console.log(i+" "+(i+2)+ " "+fichas);
+}*/
+
+//pintarCasilla(canvases[2], 4, 2); // pinta dos fichas en el 4
+//pintarCasilla(canvases[8], 11, 5); // pinta 5 fichas en el 11
+//pintarCasilla(canvases[6], 9, 3); // pinta 3 fichas en el 9
+//pintarCasilla(canvases[5], 8, 2); // pinta dos fichas en el 8
+console.log(canvases)
+
 let jugadores=0;
 let fichas=50;
 let turno =1;
 let dados =[{valor: 1}, {valor: 1}];
 let arrJugadores=["0"];
 let arrCasillas=[]
-let puchero={contenido: 0};
+let miPuchero={contenido: 0};
 let vacio=true;
+
 window.onload = function(){
     inicio();
     escribirJugadores();
@@ -27,10 +152,6 @@ function inicio() { //inicializa el juego y pide el número de jugadores
     
         crearCasillas();
 }
-
-
-
-
 function crearJugador(){ //crea los objetos de los jugadores y los añade al array
     let jugador={fichasRestantes: parseInt(fichas/jugadores),
                  fichasGanadas: 0
@@ -48,7 +169,6 @@ function escribirJugadores() { //escribe los jugadores en la pantalla
     }
     
 }
-
 function crearCasillas(){  //crea los objetos de las casillas y las añade al array
     let casilla;
     for (let i = 2; i <= 11; i++ ){
@@ -72,15 +192,13 @@ function escribirCasillas() { //escribe las casillas en la pantalla
     
     const escribirCasillas = document.getElementById('casillas');
     escribirCasillas.innerText="Casillas";
-    escribirCasillas.innerText += "\n Puchero \n" + puchero.contenido;
+    escribirCasillas.innerText += "\n Puchero \n" + miPuchero.contenido;
     for (let i=0; i<arrCasillas.length; i++){
         
         escribirCasillas.innerText +="\n Casilla " + arrCasillas[i].capacidad + "\n" + arrCasillas[i].contenido + "/" + arrCasillas[i].capacidad;
     }
     
 }
-
-
 
 function tirarDados(){ //acciones que se producen al pulsar el botón "Tirar dados"
     dados[0].valor=Math.floor(Math.random()*6+1);
@@ -100,6 +218,12 @@ function tirarDados(){ //acciones que se producen al pulsar el botón "Tirar dad
     if (arrJugadores[turno].fichasRestantes!==0){
         addFicha(suma);
         arrJugadores[turno].fichasRestantes--;
+        if (suma>7) {
+            pintarCasilla(canvases[suma-3], suma, arrCasillas[suma-3].contenido);
+        } else if(suma<7){
+            pintarCasilla(canvases[suma-2], suma, arrCasillas[suma-2].contenido);
+        }
+         
     }else{
         final(suma);
     }
@@ -112,6 +236,7 @@ function tirarDados(){ //acciones que se producen al pulsar el botón "Tirar dad
     const siguiente = document.getElementById('siguiente');
     
     siguiente.innerText= "Tira el jugador " + turno;
+    console.log(arrCasillas);
 }
 
 function addFicha(suma){ //acciones que se producen al añadir una ficha
@@ -126,12 +251,12 @@ function addFicha(suma){ //acciones que se producen al añadir una ficha
         }
     }
     if (suma==7){
-        puchero.contenido++;
+        miPuchero.contenido++;
     }
     if (suma==12){
         arrJugadores[turno].fichasRestantes++;
-        arrJugadores[turno].fichasGanadas+=puchero.contenido;
-        puchero.contenido=0;
+        arrJugadores[turno].fichasGanadas+=miPuchero.contenido;
+        miPuchero.contenido=0;
     }
     escribirCasillas();
     
@@ -143,16 +268,22 @@ function final(suma){           //acciones que se producen cuando un usuario no 
         if (suma==arrCasillas[i].capacidad){
             arrJugadores[turno].fichasGanadas+=arrCasillas[i].contenido;
             arrCasillas[i].contenido=0;
+            if (suma>7) {
+                pintarCasilla(canvases[suma-3], suma, 0);
+            } else if(suma<7){
+                pintarCasilla(canvases[suma-2], suma, 0);
+            }
         }
         if (suma==7){
-            arrJugadores[turno].fichasGanadas+=puchero.contenido;
-            puchero.contenido=0;
+            arrJugadores[turno].fichasGanadas+=miPuchero.contenido;
+            miPuchero.contenido=0;
         }
         if (suma==12){
-            arrJugadores[turno].fichasGanadas+=puchero.contenido;
+            arrJugadores[turno].fichasGanadas+=miPuchero.contenido;
             for (j=0;j<arrCasillas.length;j++){
                 arrJugadores[turno].fichasGanadas+=arrCasillas[i].contenido;
                 arrCasillas[i].contenido=0;
+                pintarCasilla(canvases[i], suma, 0);
             }
         }
     }
@@ -163,7 +294,8 @@ function final(suma){           //acciones que se producen cuando un usuario no 
 function vacioFinal() {     //comprobación de que todas las casillas están vacias al final de la partida
     vacio=true;
     for (let i=0;i<arrCasillas.length;i++){
-        if(arrCasillas[i].contenido!=0||puchero.contenido!=0){
+        if(arrCasillas[i].contenido!=0||miPuchero.contenido!=0){
+            
             vacio=false;
             
         }
@@ -174,22 +306,13 @@ function vacioFinal() {     //comprobación de que todas las casillas están vac
         document.getElementById("nuevaPartida").disabled = false;
     }
 }
-
 function nuevaPartida(){        //inicia una nueva partida
     turno =1;
     jugadores=0;
     arrJugadores=["0"];
     arrCasillas=[]
-    puchero={contenido: 0};
+    miPuchero={contenido: 0};
     inicio();
     escribirJugadores();
     escribirCasillas();
 }
-
-
-
-
-console.log(jugadores);
-console.log(arrJugadores);
-console.log(arrCasillas);
-
